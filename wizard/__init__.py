c5cba7e76ca0469a69a0c07625ba6cb048a1e6f5
ahmed <aminbouhlel041@gmail.com>
2025-07-09 11:10:24 +0100

    second commit

---
+++ wizard/__init__.py
@@ -0,0 +1,3 @@
+
+from . import mass_sms
+
---
+++ wizard/mass_sms.py
@@ -0,0 +1,93 @@
+from odoo import api, fields, models, _
+from odoo.exceptions import UserError, ValidationError
+import re
+
+
+
+class part_sms(models.TransientModel):
+    _name = 'part.tunisiesms'
+    _description = 'Part SMS'
+
+    def _default_get_gateway(self):
+        sms_obj = self.env['sms.tunisiesms']
+        gateway_ids = sms_obj.search([], limit=1)
+        return gateway_ids and gateway_ids[0] or False
+
+    @api.onchange('gateway')
+    def onchange_gateway_mass(self):
+        if not self.gateway:
+            return
+        gateway = self.gateway
+        self.validity = gateway.validity
+        self.classes = gateway.classes
+        self.deferred = gateway.deferred
+        self.priority = gateway.priority
+        self.coding = gateway.coding
+        self.tag = gateway.tag
+        self.nostop = gateway.nostop
+
+    def _merge_message(self, message, object, partner):
+        def merge(match):
+            exp = str(match.group()[2: -2]).strip()
+            result = eval(exp, {'object': object, 'partner': partner})
+            if result in (None, False):
+                return str("--------")
+            return str(result)
+        com = re.compile('(\[\[.+?\]\])')
+        msg = com.sub(merge, message)
+        return msg
+
+    def sms_mass_send(self):
+        partner_ids = []
+        for categ in self.category_id:
+            rels = self.env['res.partner'].search([('category_id', '=', categ.id)])
+            partner_ids.extend(rels.ids)
+        partner_ids = list(dict.fromkeys(partner_ids))
+        client_obj = self.env['sms.tunisiesms']
+        partner_obj = self.env['res.partner']
+        if not self.gateway:
+            raise UserError(_('You can only select one partner'))
+        for partner in partner_obj.browse(partner_ids):
+            self.mobile_to = partner.mobile
+            client_obj.send_msg(self)
+        return True
+
+    gateway = fields.Many2one('sms.tunisiesms', 'SMS Gateway', default=_default_get_gateway)
+    text  = fields.Text('Text', required=True)
+    mobile_to  = fields.Text('Text')
+    classes1  = fields.Text('Text')
+    nostop1  = fields.Text('Text')
+    validity  = fields.Integer('Validity',
+            help='The maximum time -in minute(s)- before the message is dropped')
+    classes = fields.Selection([
+                ('0', 'Flash'),
+                ('1', 'Phone display'),
+                ('2', 'SIM'),
+                ('3', 'Toolkit'),
+            ], 'Class',
+            help='The sms class: flash(0),phone display(1),SIM(2),toolkit(3)')
+    deferred = fields.Integer('Deferred',
+            help='The time -in minute(s)- to wait before sending the message')
+    priority = fields.Selection([
+                ('0', '0'),
+                ('1', '1'),
+                ('2', '2'),
+                ('3', '3')
+            ], 'Priority', help='The priority of the message')
+    coding  = fields.Selection([
+                ('1', '7 bit'),
+                ('2', 'Unicode')
+            ], 'Coding', help='The sms coding: 1 for 7 bit or 2 for unicode')
+    tag  = fields.Char('Tag',  help='An optional tag')
+
+    category_id = fields.Many2many('res.partner.category', column1='part_sms_id',
+                                    column2='category_id', string='Tags test')
+
+    nostop = fields.Selection([
+                ('0', '0'),
+                ('1', '1')
+            ], 'NoStop',
+            help='Do not display STOP clause in the message, this requires that this is not an advertising message')
+
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
